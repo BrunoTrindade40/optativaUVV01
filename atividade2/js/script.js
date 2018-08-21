cf82e725442d47e3c4b5dca0952139988920c599
BrunoTrindade40 <brunotrindade_40@hotmail.com>
2018-08-20 22:06:40 -0300

    Enviar string(andamento)

--- atividade2/js/script.js
+++ atividade2/js/script.js
@@ -22,9 +22,36 @@ var JogoDaVelha = {
 		document.getElementById("tenta-restante").innerHTML = tentativas;
 		document.getElementById("tam-palavra-escolhida").innerHTML = tamanhoPalavraEscolhida;
 
+		// document.getElementById("andamento").innerHTML = 
+		// document.getElementById("andamento")
 	}
 };
 document.addEventListener("DOMContentLoaded", function(event) {
 	JogoDaVelha.iniciar();
+
+	document.getElementById("envia-letra").addEventListener('click', function(event) {
+		event.preventDefault();
+		var inputValor = document.forms["novoForm"].elements['letra-escolhida'];
+		var valor = inputValor.value;
+
+		if (valor.length == 0) {
+			inputValor.placeholder = "Valor Inválido";
+			
+			return false;
+		}
+		inputValor.placeholder = "Valor Válido";
+		// inputValor.value = "";
+
+		return true;
+
+
+		console.log(valor);
+		console.log(valor.length);
+
+
+		// console.log(serialize(document.forms["novoForm"]));
+		// console.log("UHUUUUUUUUUUUUUU");
+	});
+
   	// console.log("DOM completamente carregado e analisado");
 });
