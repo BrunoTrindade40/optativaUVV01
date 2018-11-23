47bce6db450fce7c1385268d2cd63c35dc42740b
BrunoTrindade40 <brunotrindade_40@hotmail.com>
2018-11-23 19:14:04 -0200

    atualização ativ 8

--- atividade8/js/script.js
+++ atividade8/js/script.js
@@ -1,6 +1,24 @@
-var playerYT = {
-    stopVideo: function () {
-        player.stopVideo();
+// var classDados = {
+//     nome : '',
+//     ult_acesso : '',
+//     ult_palavra : ''
+// };
+
+var usoLocalStorage = {
+    gravar: function () {
+        var nome = document.getElementById("nome").value;
+        var ult_acesso = new Date();
+        var texto = document.getElementById("texto").value;
+
+        var txt = texto.split(" ");
+
+        console.log(nome);
+        console.log(ult_acesso);
+        console.log(texto);
+        // localStorage.setItem('user',
+        //     JSON.stringify({user: 'john', id: 10})
+        // );
+
     },
     pauseVideo: function () {
         player.pauseVideo();
@@ -15,6 +33,6 @@ var playerYT = {
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM completamente carregado e analisado");
 
-    document.getElementById("pause").addEventListener("click", playerYT.pauseVideo);
+    document.getElementById("grava").addEventListener("click", usoLocalStorage.gravar);
 
 });
